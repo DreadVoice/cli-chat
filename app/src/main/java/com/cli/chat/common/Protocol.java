5b9b1791194fa1ecfa44d516fba3a7e01c4d0ee5
122AgA5! <akashchoudhary2005@gmail.com>
2026-07-28 01:20:37 +0530

    feat: add Protocol .encode/decode over Jackson JSON

--- app/src/main/java/com/cli/chat/common/Protocol.java
+++ app/src/main/java/com/cli/chat/common/Protocol.java
@@ -1,5 +1,21 @@
 package com.cli.chat.common;
 
-public class Protocol {
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Protocol {
+    private final static ObjectMapper MAPPER = new ObjectMapper();
+
+    private Protocol() {
+        //static
+    }
+
+    public static String encode(Message message) throws IOException {
+        return MAPPER.writeValueAsString(message);
+    }
+
+    public static Message decode(String json) throws IOException {
+        return MAPPER.readValue(json, Message.class);
+    }
 }
